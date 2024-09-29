ebe3bb20e56a9c675d15a1a9c5a9c8bf3a5fa2f1
Sandeep Rana <sr36358@gmail.com>
2024-09-29 19:41:10 +0530

    Apartment Design Webpage

---
+++ Apartment Design/script.js
@@ -0,0 +1,7 @@
+// script.js
+const toggle = document.getElementById("toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
